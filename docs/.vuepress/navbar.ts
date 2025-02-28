b3f20b9540c71dc85c7dff4dacd1b1bf53a78773
YOAKE <yo2yoake@outlook.com>
2025-02-28 15:12:04 +0800

    refactor(navbar): rename notes link text

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -5,7 +5,7 @@ export const navbar = defineNavbarConfig([
   { text: '博客', link: '/blog/' },
   { text: '周记', link: '/record/README.md' },
   {
-    text: '笔记',
+    text: '知识库',
     items: [
       { text: '计算机图形学', link: '/graphics/README.md' },
       { text: 'MySQL', link: '/mysql/README.md' },
